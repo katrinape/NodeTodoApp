36883bc7be49bc0fba95f9ac96e31a5380132305
Katrinape <katrina3447e@gmail.com>
2019-05-14 17:06:35 +0200

    Quick fix

--- index.js
+++ index.js
@@ -8,42 +8,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
 var tasks = [
-  { title: "Odebrać auto z warsztatu", description: "Sprawdź czy jest naprawione!" },
-  { title: "Wizyta u stomatologa", description: "Wyrywanie ósemek..." }
+  { title: 'Odebrać auto z warsztatu', description: 'Sprawdź czy jest naprawione!' },
+  { title: 'Wizyta u stomatologa', description: 'Wyrywanie ósemek...' }
 ];
 
 var errMessage = '';
 
-app.get("/", (req, res) => res.render('index', { tasks: tasks, errMessage: errMessage }));
+app.get('/', (req, res) => res.render('index', { tasks: tasks, errMessage: errMessage }));
 
-app.post('/addtask', function (req, res) {
+app.post('/addtask', (req, res) => {
   if (!req.body.title || req.body.title.length < 10 || req.body.title.length > 255) {
     errMessage = '"Title" is required and should have more than 10 and less than 255 characters. ';
     console.log('Error message: ', errMessage);
-    return res.redirect("/");
+    return res.redirect('/');
   }
-  if (req.body.description.length > 255) {
+  if (req.body.description && req.body.description.length > 255) {
     errMessage = '"Description" should have less than 255 characters.';
     console.log('Error message: ', errMessage);
-    return res.redirect("/");
+    return res.redirect('/');
   }
   errMessage = '';
   tasks.push({ title: req.body.title, description: req.body.description });
-  return res.redirect("/");
+  return res.redirect('/');
 });
 
-app.post("/removetask", function (req, res) {
+app.post('/removetask', (req, res) => {
   errMessage = '';
   if (tasks.length > 0) tasks.pop();
   else errMessage = 'Operation not allowed. Task list is empty!';
-  return res.redirect("/");
+  return res.redirect('/');
 });
 
-app.post("/clearTasks", function (req, res) {
+app.post('/clearTasks', (req, res) => {
   errMessage = '';
   if (tasks.length > 0) tasks = [];
   else errMessage = 'Operation not allowed. Task list is empty!';
-  return res.redirect("/");
+  return res.redirect('/');
 });
 
 const port = process.env.PORT || 3000;
